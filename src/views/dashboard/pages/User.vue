ba1bddce8f8c980a6c4e3a9874297ba0bbd50cb5
supportmedrankinteractive <85926524+supportmedrankinteractive@users.noreply.github.com>
2021-06-29 14:41:06 +0800

    Update User.vue
    166 class="headline

--- src/views/dashboard/pages/User.vue
+++ src/views/dashboard/pages/User.vue
@@ -163,7 +163,7 @@
                 <div class="d-flex flex-no-wrap justify-space-between">
                   <div>
                     <v-card-title
-                      class="headline"
+                      class="text-h5"
                       v-text="item.title"
                     />
 
