4e8921e11b46521b854717cbac19755b95e80ef8
supportmedrankinteractive <85926524+supportmedrankinteractive@users.noreply.github.com>
2021-08-20 10:33:25 +0800

    Update axios-instances.js

--- src/axios-instances.js
+++ src/axios-instances.js
@@ -12,3 +12,13 @@ export const siteUrlAPI = axios.create({
   baseURL: process.env.VUE_APP_AXIOS_BASE_URL,
   withCredentials: true,
 })
+
+siteUrlAPI.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 401) {
+      this.$store.dispatch('userLogout')
+    }
+    return Promise.reject(error)
+  },
+)
